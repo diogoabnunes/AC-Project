8526b6482df18fcb08e06ae8f0e7bba15dd4c300
DanielGSilva <dg.sarmento2000@gmail.com>
2021-11-22 17:21:00 +0000

    Add processing of transactions total and per type

--- src/account.py
+++ src/account.py
@@ -26,18 +26,38 @@ def preProcessTransaction(transaction_df):
 
 preProcessedTransaction_df = preProcessTransaction(transaction_df)
 
+print(preProcessedTransaction_df.head())
+
 def processTransaction(pPTransaction_df):
     df = pPTransaction_df.copy()
     df = df.drop(['trans_id'], axis=1)
 
-    gk = df.groupby(['account_id'])
+    # Get several GroupBy Objects
+    gb = df.groupby(['account_id'])
+    gb_type = df.groupby(['account_id', 'type'])
+    gb_year = df.groupby(['account_id', 'year'])
+    gb_type_year = df.groupby(['account_id', 'type', 'year'])
     
-    counts = gk.size().to_frame(name='counts')
+    # Get statistics for balance after each transaction
+    balance_all = gb.agg({'balance': ['mean', 'min', 'max']}).droplevel(1, axis=1).set_axis(['balance_mean', 'balance_min', 'balance_max'], axis=1)
     
-    #counts.join(gk.agg({}))
+    # Get statistics for number of transactions of each type
+    no_of_type_transactions = gb_type.size().to_frame().reset_index().pivot(index='account_id', columns='type').droplevel(['type'], axis=1).set_axis(['debits', 'credits'], axis=1)
+    
+    # Get statistics for transaction amount and balance after transaction for each type
+    amount_balance_type = gb_type.agg({'amount': ['sum', 'mean', 'min', 'max'], 'balance': ['mean', 'min', 'max']}).reset_index().pivot(index='account_id', columns='type').droplevel([0, 'type'], axis=1).set_axis(['amount_sum_debit', 'amount_sum_credit', 'amount_mean_debit', 'amount_mean_credit', 'amount_min_debit', 'amount_min_credit', 'amount_max_debit', 'amount_max_credit', 'balance_mean_debit', 'balance_mean_credit', 'balance_min_debit', 'balance_min_credit', 'balance_max_debit', 'balance_max_credit'], axis=1)
+    
+    # Get new dataframe with number of transactions for each account
+    counts = gb.size().to_frame(name='no. of transactions')
+    
+    # Join all the data for each account
+    counts = counts.join(balance_all).join(no_of_type_transactions).join(amount_balance_type).reset_index()
+    
+    # TODO stats per year
+    # TODO stats per type per year
     
     return counts
     
 processedTransaction_df = processTransaction(preProcessedTransaction_df)
 
-print(processedTransaction_df.head())
+print(processedTransaction_df)
