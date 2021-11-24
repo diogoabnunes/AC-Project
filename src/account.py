6b413c50c4f3d99e76eb206a55e6e34da783d5da
DanielGSilva <dg.sarmento2000@gmail.com>
2021-11-24 16:58:12 +0000

    Finish transaction processing

--- src/account.py
+++ src/account.py
@@ -36,7 +36,7 @@ def processTransaction(pPTransaction_df):
     gb = df.groupby(['account_id'])
     gb_type = df.groupby(['account_id', 'type'])
     gb_year = df.groupby(['account_id', 'year'])
-    gb_type_year = df.groupby(['account_id', 'type', 'year'])
+    gb_year_type = df.groupby(['account_id', 'year', 'type'])
     
     # Get statistics for balance after each transaction
     balance_all = gb.agg({'balance': ['mean', 'min', 'max']}).droplevel(1, axis=1).set_axis(['balance_mean', 'balance_min', 'balance_max'], axis=1)
@@ -47,14 +47,23 @@ def processTransaction(pPTransaction_df):
     # Get statistics for transaction amount and balance after transaction for each type
     amount_balance_type = gb_type.agg({'amount': ['sum', 'mean', 'min', 'max'], 'balance': ['mean', 'min', 'max']}).reset_index().pivot(index='account_id', columns='type').droplevel([0, 'type'], axis=1).set_axis(['amount_sum_debit', 'amount_sum_credit', 'amount_mean_debit', 'amount_mean_credit', 'amount_min_debit', 'amount_min_credit', 'amount_max_debit', 'amount_max_credit', 'balance_mean_debit', 'balance_mean_credit', 'balance_min_debit', 'balance_min_credit', 'balance_max_debit', 'balance_max_credit'], axis=1)
     
+    # Get statistics for number of transactions of each year
+    no_of_year_transactions = gb_year.size().to_frame().reset_index().pivot(index='account_id', columns='year').droplevel(['year'], axis=1).set_axis(['no. of transactions 1993', 'no. of transactions 1994', 'no. of transactions 1995', 'no. of transactions 1996'], axis=1).fillna(0)
+    
+    # Get statistics for balance after transaction for each year
+    balance_all_year = gb_year.agg({'balance': ['mean', 'min', 'max']}).reset_index().pivot(index='account_id', columns='year').droplevel([0, 1], axis=1).set_axis(['balance_mean_1993', 'balance_mean_1994', 'balance_mean_1995', 'balance_mean_1996', 'balance_min_1993', 'balance_min_1994', 'balance_min_1995', 'balance_min_1996', 'balance_max_1993', 'balance_max_1994', 'balance_max_1995', 'balance_max_1996'], axis=1)
+    
+    # Get statistics for number of transactions of each type in each year
+    no_of_year_type_transactions = gb_year_type.size().to_frame().reset_index().pivot(index='account_id', columns=['year', 'type']).droplevel([0, 1], axis=1).set_axis(['1995_debits', '1995_credits', '1996_debits', '1996_credits', '1993_debits', '1993_credits', '1994_debits', '1994_credits'], axis=1)
+    
+    # Get statistics for transaction amount and balance after transaction for each type in each year
+    amount_balance_year_type = gb_year_type.agg({'amount': ['sum', 'mean', 'min', 'max'], 'balance': ['mean', 'min', 'max']}).reset_index().pivot(index='account_id', columns=['year', 'type']).droplevel([0, 1], axis=1).set_axis(['amount_sum_1993_debit', 'amount_sum_1993_credit', 'amount_sum_1994_debit', 'amount_sum_1994_credit', 'amount_sum_1995_debit', 'amount_sum_1995_credit', 'amount_sum_1996_debit', 'amount_sum_1993_credit', 'amount_mean_1993_debit', 'amount_mean_1993_credit', 'amount_mean_1994_debit', 'amount_mean_1994_credit', 'amount_mean_1995_debit', 'amount_mean_1995_credit', 'amount_mean_1996_debit', 'amount_mean_1996_credit', 'amount_min_1993_debit', 'amount_min_1993_credit', 'amount_min_1994_debit', 'amount_min_1994_credit', 'amount_min_1995_debit', 'amount_min_1995_credit', 'amount_min_1996_debit', 'amount_min_1996_credit', 'amount_max_1993_debit', 'amount_max_1993_credit', 'amount_max_1994_debit', 'amount_max_1994_credit', 'amount_max_1995_debit', 'amount_max_1995_credit', 'amount_max_1996_debit', 'amount_max_1996_credit', 'balance_mean_1993_debit', 'balance_mean_1993_credit', 'balance_mean_1994_debit', 'balance_mean_1994_credit', 'balance_mean_1995_debit', 'balance_mean_1995_credit', 'balance_mean_1996_debit', 'balance_mean_1996_credit', 'balance_min_1993_debit', 'balance_min_1993_credit', 'balance_min_1994_debit', 'balance_min_1994_credit', 'balance_min_1995_debit', 'balance_min_1995_credit', 'balance_min_1996_debit', 'balance_min_1996_credit', 'balance_max_1993_debit', 'balance_max_1993_credit', 'balance_max_1994_debit', 'balance_max_1994_credit', 'balance_max_1995_debit', 'balance_max_1995_credit', 'balance_max_1996_debit', 'balance_max_1996_credit'], axis=1)
+    
     # Get new dataframe with number of transactions for each account
     counts = gb.size().to_frame(name='no. of transactions')
     
     # Join all the data for each account
-    counts = counts.join(balance_all).join(no_of_type_transactions).join(amount_balance_type).reset_index()
-    
-    # TODO stats per year
-    # TODO stats per type per year
+    counts = counts.join(balance_all).join(no_of_type_transactions).join(amount_balance_type).join(no_of_year_transactions).join(balance_all_year).join(no_of_year_type_transactions).join(amount_balance_year_type).reset_index()
     
     return counts
     
